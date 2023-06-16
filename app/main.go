aa5f87e5a89e1d9287cbae066ac0cdab170e6ee7
Nil Borodulia <nil.b@oxtech.org>
2023-06-16 13:05:38 +0300

    add reader config file

--- app/main.go
+++ app/main.go
@@ -4,10 +4,20 @@ import (
 	"fmt"
 	"github.com/jessevdk/go-flags"
 	"log"
+	"os"
+	"gopkg.in/yaml.v3"
 )
 
 
 var revision string = "1.0"
+
+type Listener struct {
+    Containers []struct {
+        Name string `yaml:"name"`
+        Regexp string `yaml:"regexp"`
+        Label string `yaml:"label"`
+    } `yaml:"containers"`
+}
 
 type Options struct {
 	Config string  `short:"f" long:"file" env:"CONF" default:"listener.yml" description:"config file"`
@@ -24,4 +34,26 @@ func main() {
     }
 
     fmt.Printf(opts.Config)
+
+    listener, errYaml := LoadConfig(opts.Config)
+    if errYaml != nil {
+        log.Println(errYaml)
+    }
+
+    fmt.Println(listener)
 }
+
+func LoadConfig(file string) (*Listener, error) {
+	fh, err := os.Open(file) //nolint
+	if err != nil {
+		return nil, fmt.Errorf("can't load config file %s: %w", file, err)
+	}
+	defer fh.Close() //nolint
+
+	res := Listener{}
+	if err := yaml.NewDecoder(fh).Decode(&res); err != nil {
+		return nil, fmt.Errorf("can't parse config: %w", err)
+	}
+	return &res, nil
+}
+
