2cb4e67e622c0684029530ebfcaccdf5b75ffab9
Nil Borodulia <nil.b@oxtech.org>
2023-11-23 10:48:45 +0300

    Add listener configuration and handle client connection

--- app/server.go
+++ app/server.go
@@ -4,6 +4,10 @@ import (
 	"encoding/gob"
 	"fmt"
 	"net"
+	"gopkg.in/yaml.v3"
+	"github.com/jessevdk/go-flags"
+	"log"
+	"time"
 )
 
 type ContainerMessages struct {
@@ -17,8 +21,65 @@ type Message struct {
 	Message string
 }
 
+var revision string = "1.0"
+
+type Listener struct {
+    Containers []struct {
+        Name string `yaml:"name"`
+        Regexp []string `yaml:"regexp"`
+        Label string `yaml:"label"`
+        All bool `yaml:"all"`
+    } `yaml:"containers"`
+}
+
+type Options struct {
+	Config string  `short:"f" long:"file" env:"CONF" default:"listener.yml" description:"config file"`
+	Port string `short:"p" long:"port" env:"PORT" default:"2323" description:"port"`
+}
+
+func main() {
+    fmt.Printf("Listener %s\n", revision)
+
+    var opts Options
+    parser := flags.NewParser(&opts, flags.Default)
+    _, err := parser.Parse()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    listener, errYaml := LoadConfig(opts.Config)
+    if errYaml != nil {
+        log.Println(errYaml)
+    }
+
+	// Start server
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+		return
+	}
+	defer listener.Close()
+	fmt.Println("Server started. Waiting for clients...")
+
+    tmpTime := time.Now().Add(-time.Second * 10).Format("2006-01-02T15:04:05")
+
+	// Accept client connections
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			continue
+		}
+		fmt.Println("Client connected:", conn.RemoteAddr())
+
+		// Handle client connection in a goroutine
+		go handleClientConnection(conn)
+	}
+}
+
 func handleClientConnection(conn net.Conn) {
 	defer conn.Close()
+
 
 	// Create a new encoder to send gob data
 	encoder := gob.NewEncoder(conn)
@@ -38,29 +99,5 @@ func handleClientConnection(conn net.Conn) {
 	if err != nil {
 		fmt.Println("Error encoding and sending data:", err)
 		return
-	}
-}
-
-func main() {
-	// Start server
-	listener, err := net.Listen("tcp", ":8080")
-	if err != nil {
-		fmt.Println("Error starting server:", err)
-		return
-	}
-	defer listener.Close()
-	fmt.Println("Server started. Waiting for clients...")
-
-	// Accept client connections
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			fmt.Println("Error accepting connection:", err)
-			continue
-		}
-		fmt.Println("Client connected:", conn.RemoteAddr())
-
-		// Handle client connection in a goroutine
-		go handleClientConnection(conn)
 	}
 }
